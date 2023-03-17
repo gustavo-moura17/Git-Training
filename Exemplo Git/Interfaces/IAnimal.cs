4ccf298d5e5e0788785f49272748216479f0e7eb
GustavoHRM <gustavohrm17@gmail.com>
2023-03-17 10:12:26 -0300

    Added Animal Interface

---
+++ Exemplo Git/Interfaces/IAnimal.cs
@@ -0,0 +1,12 @@
+﻿namespace Exemplo_Git.Interfaces
+{
+    public interface IAnimal
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public abstract string PorteDoAnimal();
+    }
+}
