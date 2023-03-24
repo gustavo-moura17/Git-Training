872a7dc7398356dfd64ea90ae8af73b267590f1a
Raphael Furlan Grivol <raphael.furlan@mapdata.com.br>
2023-03-24 16:51:53 -0300

    Criado Ornitorrinco

---
+++ Exemplo Git/Models/Ornitorrinco.cs
@@ -0,0 +1,30 @@
+﻿using Exemplo_Git.Interfaces;
+
+namespace Exemplo_Git.Models
+{
+    public class Ornitorrinco
+    {
+            public int ID { get; set; }
+            public string Nome { get; set; }
+            public double Peso { get; set; }
+            public double Altura { get; set; }
+
+        public string PorteDoAnimal()
+        {
+            string porte = "";
+            if (Altura >= .8)
+            {
+                porte = "Grande";
+            }
+            else if (Altura < .8 && Altura >= .4)
+            {
+                porte = "Médio";
+            }
+            else if (Altura < .4)
+            {
+                porte = "Pequeno";
+            }
+            return porte;
+        }
+    }
+}
